fad4269b94b60cebd37f606fec7dfde9fc524e4e
Fei Pan <feipanir@gmail.com>
2024-06-11 13:05:50 -0400

    upload files

---
+++ motion_training/datasets/__init__.py
@@ -0,0 +1,5 @@
+# for the training process
+from .custom_mono_dataset import CustomMonoDataset
+from .custom_mono_dataset_city import CustomMonoDataset as City
+from .mixed_mono_dataset import MixedMonoDataset
+from .auxiliary_datasets import ProcessedImageFolder
